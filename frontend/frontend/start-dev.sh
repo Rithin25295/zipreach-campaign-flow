0d480d7965f090e17777b91cb57ca493a4a89fe3
Rithin25295 <Rithin25295@gmail.com>
2025-06-27 13:09:54 -0500

    fix: Move files to correct location

--- frontend/frontend/start-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script passes system environment variables to Vite
-source ~/.zshrc
-
-# Pass the environment variables to the Vite dev server
-VITE_SUPABASE_URL=$SUPABASEURLZR \
-VITE_SUPABASE_API_KEY=$SUPABASEAPIKEYZR \
-npm run dev
